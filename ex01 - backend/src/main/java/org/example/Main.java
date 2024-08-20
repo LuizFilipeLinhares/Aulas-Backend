f2cda9edf740f1838fc656f24814c453c7ddb7c5
Luiz Filipe Linhares <137822321+LuizFilipeLinhares@users.noreply.github.com>
2024-08-20 20:57:57 -0300

    Update Main.java
    Ajustando o exercicio

--- ex01 - backend/src/main/java/org/example/Main.java
+++ ex01 - backend/src/main/java/org/example/Main.java
@@ -6,31 +6,23 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args){
+        String[]  = {"MIB", "Arrive","Black Knight","Gladiator"};
+        Scanner leitor = new Scanner(System.in);
 
-        String[]  = {"Agua", "Fogo","Terra","Ar"};
 
         System.out.println("Selecione a ambientação do seu filme:");
         System.out.println("1 - Sci - fi");
-        System.out.println("1 - Medieval");
+        System.out.println("2 - Medieval");
+        String opcaoambiente = leitor.nextInt();
+
+        System.out.println("Selecione o genero do seu filme:");
+        System.out.println("1 - Comedia");
+        System.out.println("1 - Drama");
+        String opcaoGenero = leitor.nextInt();
 
 
-
-        Scanner scanner = new Scanner(System.in);
-
-        //System.out.println("Digite o primeiro nome:");
-        /*String firstName = scanner.nextLine();
-
-        if(firstName == "") return;
-
-        System.out.println("Digite o segundo nome:");
-        String secondName = scanner.nextLine();*/
-
-        Random aleatoryObj = new Random();
-        Integer aleatoryNumber = aleatoryObj.nextInt( 99);
-        Integer aleatoryInd = aleatoryObj.nextInt(4);
-        String element = elements[aleatoryInd];
-
-        System.out.println("Eu remocomendaria o: " + element);
     }
 }
+
+
 
