7eef26dd96674374a044a18b7dd7329afd16f41f
Luiz Filipe Romualdo Linhares <luiz.288322@alunosatc.edu.br>
2024-08-26 20:20:42 -0300

    fazendo parte do ex03

---
+++ Exercicios de Avaliacao/Ex - 003 av/src/main/java/org/example/Barco.java
@@ -0,0 +1,11 @@
+package org.example;
+
+public class Boat {
+    public String name;
+    public integer size;
+
+    public Boat(String name, Integer size) {
+        this.name = name;
+        this.size = size;
+    }
+}
---
+++ Exercicios de Avaliacao/Ex - 003 av/src/main/java/org/example/BasePorto.java
@@ -0,0 +1,12 @@
+package org.example;
+
+import java.lang.reflect.Array;
+
+
+public class baseHarbor {
+    public String name;
+
+
+
+
+}
