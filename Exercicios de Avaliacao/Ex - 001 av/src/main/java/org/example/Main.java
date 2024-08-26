27f8be9451ed73bf83960e2aa93eb5e51da28e69
Luiz Filipe Romualdo Linhares <luiz.288322@alunosatc.edu.br>
2024-08-26 20:17:31 -0300

    Ajustando o exercicio 1

--- Exercicios de Avaliacao/Ex - 001 av/src/main/java/org/example/Main.java
+++ Exercicios de Avaliacao/Ex - 001 av/src/main/java/org/example/Main.java
@@ -6,23 +6,26 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args){
-        String[]  = {"MIB", "Arrive","Black Knight","Gladiator"};
-        Scanner leitor = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
+        String[] movies = {"MIB", "Arrive","Black Knight","Gladiator"};
 
 
         System.out.println("Selecione a ambientação do seu filme:");
         System.out.println("1 - Sci - fi");
         System.out.println("2 - Medieval");
-        String opcaoambiente = leitor.nextInt();
+        String ambianceOption = scanner.nextInt();
 
         System.out.println("Selecione o genero do seu filme:");
         System.out.println("1 - Comedia");
         System.out.println("1 - Drama");
-        String opcaoGenero = leitor.nextInt();
+        String genOption = scanner.nextInt();
 
+        String filmeRecomendado = movies[ambianceOption + genOption - 2];
 
+        System.out.println(String.format("Eu recomendo o filme: %s", filmeRecomendado));
     }
 }
+
 
 
 
